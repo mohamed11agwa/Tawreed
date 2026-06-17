c72a348285e8c4415776ab4b9ccb2ac30f8e412b
ameen226 <hosan2662000@gmail.com>
2026-06-17 03:09:13 +0300

    feat: change Supplier-Region relationship from one-to-many to many-to-many

--- Tawreed.DAL/Migrations/20260616183710_fixing_region_supplier_relationship.Designer.cs
+++ Tawreed.DAL/Migrations/20260616183710_fixing_region_supplier_relationship.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tawreed.DAL.Data;
 
@@ -11,9 +12,11 @@ using Tawreed.DAL.Data;
 namespace Tawreed.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260616183710_fixing_region_supplier_relationship")]
+    partial class fixing_region_supplier_relationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
